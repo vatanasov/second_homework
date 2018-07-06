5c2b42b5f777a60f3dd332c71f99cda658d13a05
Dimitar Ivanov <40768195+mitko96@users.noreply.github.com>
2018-07-06 10:41:28 +0300

    Add files via upload

---
+++ homework2.ex
@@ -0,0 +1,26 @@
+defmodule Homework do
+
+  def first_task() do
+    # Using a pattern match, assign "Alice" to a variable "first_name", and "Addison" to variable "last_name"
+
+ {first_name, last_name}= {"Alice", "Addison"}
+	
+    # Assign the number to the variable "age", and the letter to "sex"
+   
+    {age,sex} = {32, "f"}
+   
+    # Create a patter, that will fail if the first item of the tuple is anything other than "200"
+  {_,y,z} = {200, "example data", :an_atom}
+
+    # From here on, do not use variables. All of these are heavily inspired by elixirkoans
+    true = true
+    false = !true
+    2 = 1 + 1
+   4.0 = 2 / 0.5
+    6 = 2 * 3
+    3 > 2
+    "hello world" = "hello " <> "world"
+    list = [1, 2] ++ [:a, "b"]
+    list1 = [1, 2, 3] -- [3]
+  end
+end
