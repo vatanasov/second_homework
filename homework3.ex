853e0ea40d2f857dacedf0067885bd306c706964
vatanasov <atanasov.valentin@yahoo.com>
2018-07-12 14:53:29 +0300

    Fixes
    Missing that coma would lead to syntax error.
    For the second task, the pattern you had wrote would only match list with 1 element, it should match anything, that is not an empty list.

--- homework3.ex
+++ homework3.ex
@@ -2,7 +2,7 @@
   
   buzz = fn 
 	{0,0,_} -> "FizzBuzz"
-	{0_,_} -> "Fizz"
+	{0,_,_} -> "Fizz"
 	{_,0,_} -> "Buzz"
 	{_,_,text} -> text
   end
@@ -12,7 +12,7 @@
 
   list_empty = fn
 	[] -> "true"
-	[_] -> "false"
+	_ -> "false"
   end
 
 
