fb019038374e205e9115350ea4007fabdd755bef
Dimitar Ivanov <40768195+mitko96@users.noreply.github.com>
2018-07-12 09:55:25 +0300

    Add files via upload

---
+++ homework3.ex
@@ -0,0 +1,20 @@
+# 1 zadacha
+  
+  buzz = fn 
+	{0,0,_} -> "FizzBuzz"
+	{0_,_} -> "Fizz"
+	{_,0,_} -> "Buzz"
+	{_,_,text} -> text
+  end
+
+
+#2 zadacha
+
+  list_empty = fn
+	[] -> "true"
+	[_] -> "false"
+  end
+
+
+
+
